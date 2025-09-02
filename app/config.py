706be113befabef496e83c70e7e0962119735051
somoscddtecnologia <somoscddtecnologia@gmail.com>
2025-09-01 22:02:19 -0300

    ACCESS_TOKEN_EXPIRE_MINUTES 1

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 # app/config.py
 MONGO_URL = "mongodb://localhost:27017"
 MONGO_DB = "calculadoraDB"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 SECRET_KEY = "GELSON"
 ALGORITHM = "HS256"
