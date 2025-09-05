0851f791cad17254eabef685784beded18f07e9b
somoscddtecnologia <somoscddtecnologia@gmail.com>
2025-09-04 21:17:57 -0300

    add autenticação

--- app/routers/calculadora.py
+++ app/routers/calculadora.py
@@ -15,26 +15,26 @@ def somar(a:float, b:float, usuario=Depends(get_usuario_atual)):
     return {"resultado": a+b} 
 
 @router.post("/subtrair")
-def subtrair(dados: OperacaoDoisNumeros):
+def subtrair(dados: OperacaoDoisNumeros, usuario=Depends(get_usuario_atual)):
     return {"resultado": dados.a - dados.b}
 
 @router.post("/multiplicar")
-def multiplicar(dados: OperacaoDoisNumeros):
+def multiplicar(dados: OperacaoDoisNumeros, usuario=Depends(get_usuario_atual)):
     return {"resultado": dados.a * dados.b}
 
 @router.post("/dividir")
-def dividir(dados: OperacaoDoisNumeros):
+def dividir(dados: OperacaoDoisNumeros, usuario=Depends(get_usuario_atual)):
     if dados.b == 0:
         raise HTTPException(status_code=400, detail="Não é permitido dividir por zero")
     return {"resultado": dados.a / dados.b}
 
 @router.post("/potencia")
-def potencia(dados: OperacaoDoisNumeros):
+def potencia(dados: OperacaoDoisNumeros, usuario=Depends(get_usuario_atual)):
     #return {"resultado": math.pow(dados.a, dados.b)}
     return {"resultado": dados.a ** dados.b}
 
 @router.post("/raiz")
-def raiz(dados: OperacaoRaiz):
+def raiz(dados: OperacaoRaiz, usuario=Depends(get_usuario_atual)):
     if dados.indice == 0:
         raise HTTPException(status_code=400, detail="Não é permitido indice zero")
     return {"resultado": dados.numero ** (1/dados.indice)}
